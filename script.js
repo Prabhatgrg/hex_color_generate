0cad6836785696cd380ff158ccece8e3789858f7
prabhatgrg <prabhatgurung34@gmail.com>
2022-02-26 20:58:26 +0545

    Color Generator works need to work on css

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
 
 var hex = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
 
-
-const change = document.getElementById("changer");
-const color_name = document.querySelector(".color");
+const change = document.getElementById("btn");
+const color = document.querySelector(".color");
 
 change.addEventListener('click', function(){
     let hex_color = "#";
-    for(i=0;i<6;i++){
-        hex_color += hex[random_number];
+    for(let i=0;i<6;i++){
+        hex_color += hex[random_number()];
     }
 
-    color_name.textContent = hex_color;
+    color.textContent = hex_color;
     document.body.style.backgroundColor = hex_color;
-})
+});
 
 function random_number()
 {
-    return Math.floor(Math.random()*hex.length);
+    return Math.floor(Math.random() * hex.length);
 }
